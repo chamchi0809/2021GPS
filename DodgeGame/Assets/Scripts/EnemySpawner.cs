1904d19d2a48dc0b55e354217db49ecf3929cf1f
seoshi1234 <seoshi999@gmail.com>
2021-04-02 17:08:38 +0900

    심민성 피하기

---
+++ DodgeGame/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Enemy enemy;
+    public GameObject enemyGroup;
+    bool isPlaying = true;
+
+    public void OffSpawner()
+    {
+        isPlaying = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaying)
+        {
+            int spawnPer = Random.Range(0, 1001);
+            if (spawnPer < 50)
+            {
+                Enemy e = Instantiate(enemy);
+
+                float posY = Random.Range(-5f, 5f);
+                int isLeftInstantiate = Random.Range(0, 2);
+
+                if (isLeftInstantiate == 0)
+                {
+                    e.transform.position = new Vector3(-9.7f, posY);
+                    e.transform.localScale = new Vector2(5, 5);
+                    e.SetDirectionVector(1);
+                }
+                else
+                {
+                    e.transform.position = new Vector3(9.7f, posY);
+                    e.transform.localScale = new Vector2(-5, 5);
+                    e.SetDirectionVector(-1);
+                }
+                e.transform.parent = enemyGroup.transform;
+            }
+        }
+    }
+}
---
+++ DodgeGame/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Vector2 moveInput;
+    public float moveForce;
+    public float drag;
+
+    int hp = 3;
+
+    public int GetHP()
+    {
+        return hp;
+    }
+
+    public void SetHP(int hp)
+    {
+        this.hp = hp;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+            return;
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized; 
+    }
+    void FixedUpdate()
+    {
+        if (hp > 0)
+            rb.AddForce(moveInput * moveForce);
+        rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, drag);
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {            
+            print(--hp);
+        }
+    }
+}
